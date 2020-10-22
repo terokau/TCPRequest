1f7e23a8dfbc07b6e90cdff02e2935015a978353
Tero Kauria <tero.kauria@live.com>
2020-10-22 10:06:33 +0300

    s

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import socketserver as soc
 import sys
 
 def main():
-	host,port = '192.168.1.130',60000
+	host,port = '192.168.1.2',60000
 	
 	with soc.TCPServer((host,port),TCPHandle) as server:
 		server.serve_forever()
@@ -24,9 +24,10 @@ class MyRequest():
 	
 	def getRequestData(self,setUrl):
 		self.setUrl = str(setUrl,'utf-8')
-		sgetResponse = requests.get(self.setUrl)
+		
 		try:
-			return sgetResponse.text
+			getResponse = requests.get(self.setUrl)
+			return getResponse.text
 		except Exception as e:
 			print(e)
 			return 'Error on Request, ' 
