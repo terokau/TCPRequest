393ac52366ab734eb2afbdaa3859524bfc251591
Tero Kauria <tero.kauria@omron.com>
2020-10-23 10:18:40 +0300

    .

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ import xml.etree.ElementTree as ET
 import sys
 
 def main():
-	
+
 	settings = ET.parse('conf.xml')
 	root = settings.getroot()
-	for i in root.findall('host'):
-		host = i.find('address').text
-		port = int(i.find('port').text)
-	print('Starting open of server: ' , host, ' : ' , port)
 	
+	for child in root.findall('address'):
+		host = child.text
+
+	for child in root.findall('port'):
+		port = int(child.text)
+
+	#host = root.find('address').text
+	#port = int(root.find('port').text)
+	#host = 'localhost'
+	#port = 9000
+	print('Starting open of server: ' , host, ' : ' , port)
+
 	with soc.TCPServer((host,port),TCPHandle) as server:
 		server.serve_forever()
-		
+
 
 class TCPHandle(soc.BaseRequestHandler):
-	
+
 	def handle(self):
 		conn = MyRequest()
 		self.data = self.request.recv(2048).strip()
@@ -26,21 +34,19 @@ class TCPHandle(soc.BaseRequestHandler):
 		self.response = conn.getRequestData(self.data)
 		print('Reponse: ' , self.response)
 		self.request.sendall(str.encode(self.response))
-		
+
 class MyRequest():
-	
-	
+
+
 	def getRequestData(self,setUrl):
 		self.setUrl = str(setUrl,'utf-8')
-		
+
 		try:
 			getResponse = requests.get(self.setUrl)
 			return getResponse.text
 		except Exception as e:
 			print(e)
-			return 'Error on Request, ' 
-		
+			return 'Error on Request, '
+
 if __name__ == '__main__':
 	main()
-	
-	
