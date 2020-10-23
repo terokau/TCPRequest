666f466080198e46e3d0a117e53454eebe7956e1
Tero Kauria <tero.kauria@live.com>
2020-10-23 12:38:37 +0300

    added host to config.xml. This will be white list of allowed clients

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ import xml.etree.ElementTree as ET
 import sys
 
 def main():
-
 	settings = ET.parse('conf.xml')
 	root = settings.getroot()
-	allowedHosts = []
-	
+
 	host = root.find('address').text
 	port = int(root.find('port').text)
-	hosts = root.find('hosts')
-	for i in hosts.findall('host'):
-		print(i.get('address'))
-		allowedHosts.append(i.attrib)
+	
+	
 	print('Starting open of server: ' , host, ' : ' , port)
 
 	with soc.TCPServer((host,port),TCPHandle) as server:
@@ -24,9 +20,11 @@ def main():
 class TCPHandle(soc.BaseRequestHandler):
 
 	def handle(self):
+
 		conn = MyRequest()
 		getClientAddress = self.client_address[0]
-		if(getClientAddress in allowedHosts):
+		valid =self.getAllowedHosts(getClientAddress)
+		if(valid):
 			self.data = self.request.recv(2048).strip()
 			print('From: ' , format(self.client_address[0]))
 			print('Data: ' , self.data)
@@ -35,6 +33,22 @@ class TCPHandle(soc.BaseRequestHandler):
 			self.request.sendall(str.encode(self.response))
 		else:
 			print("not allowerd from: " + getClientAddress)
+			self.request.sendall(str.encode('Not allowed from this IP-address ' + format(self.client_address[0])))
+
+	def getAllowedHosts(self,address):
+		settings = ET.parse('conf.xml')
+		root = settings.getroot()
+		allowedHosts = []
+		hosts = root.find('hosts')
+
+		for i in hosts.findall('host'):
+			print(i.text)
+			allowedHosts.append(i.text)
+
+		if(address in allowedHosts):
+			return True
+		else:
+			return False
 
 class MyRequest():
 
